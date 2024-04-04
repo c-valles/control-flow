6b9b05d80fba1ad78833bab7ed7dbf31dddebce2
Cynthia Valles <cynthiavalles8299@gmail.com>
2024-04-04 14:20:44 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//-----Part 1: Growing Pains-----//
+
+const radius = 5;
+const pi = 3.1415;
+const minSpaceForPlants = 0.8;
+const initialPlants = 20;
+const capacity = (pi * radius * radius / minSpaceForPlants); 
